4fd2c4a05bc13eb5d9e6bf1e6e3296eee635176f
Fabrício Justo <fajusto@icloud.com>
2025-06-16 10:30:09 -0300

    fx: removido limpeza

--- packages/dart_pptx/lib/src/archive/create_zip.dart
+++ packages/dart_pptx/lib/src/archive/create_zip.dart
@@ -3,29 +3,31 @@ import 'zip_file_encoder.dart';
 List<int>? createZip(Map<String, dynamic> files) {
   final encoder = ZipFileEncoder();
   final tempDir = fs.systemTempDirectory.createTempSync();
+
   for (final entry in files.entries) {
     final name = entry.key;
     final value = entry.value;
+    final file = fs.file('${tempDir.path}/$name');
+    if (!file.existsSync()) {
+      file.createSync(recursive: true);
+    }
     if (value is List<int>) {
-      final file = fs.file('${tempDir.path}/$name');
-      if (!file.existsSync()) {
-        file.createSync(recursive: true);
-      }
       file.writeAsBytesSync(value);
     } else if (value is String) {
-      final file = fs.file('${tempDir.path}/$name');
-      if (!file.existsSync()) {
-        file.createSync(recursive: true);
-      }
       file.writeAsStringSync(value);
     }
   }
-  final outFile = fs.file('${tempDir.path}/presentation.pptx');
+
+  // 🔄 Salvar o .pptx FINAL FORA do tempDir para evitar auto-inclusão
+  final output = fs.systemTempDirectory.createTempSync();
+  final outFile = fs.file('${output.path}/presentation.pptx');
+
   encoder.zipDirectory(
     tempDir,
     filename: outFile.path,
   );
+
   final bytes = outFile.readAsBytesSync();
-  tempDir.deleteSync(recursive: true);
+
   return bytes;
 }
