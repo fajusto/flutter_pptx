7c4ca34f0fe9c2bfc02691955e678fb23f2c0ad0
fajusto <fajusto@icloud.com>
2026-01-14 17:34:20 -0300

    alteração atoa

--- packages/flutter_pptx/lib/flutter_pptx.dart
+++ packages/flutter_pptx/lib/flutter_pptx.dart
@@ -5,5 +5,6 @@ export 'src/context.dart';
 export 'src/assets.dart';
 export 'src/hex_color.dart';
 export 'src/image.dart';
+
 export 'src/powerpoint.dart';
 export 'package:dart_pptx/dart_pptx.dart' hide PowerPoint;
