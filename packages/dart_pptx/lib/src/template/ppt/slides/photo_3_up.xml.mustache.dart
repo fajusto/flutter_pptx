44160b9e963b85152400951b3d1f37594cfa0874
fajusto <fajusto@icloud.com>
2026-05-21 10:01:46 -0300

    fix: ajuste no título

--- packages/dart_pptx/lib/src/template/ppt/slides/photo_3_up.xml.mustache.dart
+++ packages/dart_pptx/lib/src/template/ppt/slides/photo_3_up.xml.mustache.dart
@@ -1,15 +1,13 @@
-/// This file is generated, do not edit!
+﻿/// This file is generated, do not edit!
 /// See `bin/copy.dart` for more information.
 
 /// Path: ./template\ppt\slides\photo_3_up.xml.mustache
-const String template = r'''/// This file is generated, do not edit!
-/// See `bin/copy.dart` for more information.
-
-/// Path: ./template/ppt/slides/photo_3_up.xml.mustache
 const String template = r'''<?xml version="1.0" encoding="UTF-8"?>
 <p:sld xmlns:a="http://schemas.openxmlformats.org/drawingml/2006/main"
     xmlns:r="http://schemas.openxmlformats.org/officeDocument/2006/relationships"
-    xmlns:p="http://schemas.openxmlformats.org/presentationml/2006/main">
+    xmlns:p="http://schemas.openxmlformats.org/presentationml/2006/main"
+    xmlns:m="http://schemas.openxmlformats.org/officeDocument/2006/math"
+    xmlns:a14="http://schemas.microsoft.com/office/drawing/2010/main" showMasterSp="1" showMasterPhAnim="1">
     <p:cSld>
         {{>slide-background}}
         <p:spTree>
@@ -53,14 +51,6 @@ const String template = r'''<?xml version="1.0" encoding="UTF-8"?>
                 </p:blipFill>
                 <p:spPr>
                   <a:xfrm>
-<<<<<<< Updated upstream
-                    <a:off x="1772330" y="722295"/>
-                    <a:ext cx="6567291" cy="11020500"/>
-                  </a:xfrm>
-                  <a:prstGeom prst="rect">
-                    <a:avLst/>
-                  </a:prstGeom>
-=======
                     <a:off x="1772330" y="2540000"/>
                     <a:ext cx="6200000" cy="9500000"/>
                   </a:xfrm>
@@ -70,7 +60,6 @@ const String template = r'''<?xml version="1.0" encoding="UTF-8"?>
                   <a:ln w="50800">
                     <a:noFill/>
                   </a:ln>
->>>>>>> Stashed changes
                 </p:spPr>
             </p:pic>
             <!-- Photo 2 -->
@@ -99,14 +88,6 @@ const String template = r'''<?xml version="1.0" encoding="UTF-8"?>
                 </p:blipFill>
                 <p:spPr>
                   <a:xfrm>
-<<<<<<< Updated upstream
-                    <a:off x="9028516" y="722295"/>
-                    <a:ext cx="6567291" cy="11020500"/>
-                  </a:xfrm>
-                  <a:prstGeom prst="rect">
-                    <a:avLst/>
-                  </a:prstGeom>
-=======
                     <a:off x="9028516" y="2540000"/>
                     <a:ext cx="6200000" cy="9500000"/>
                   </a:xfrm>
@@ -116,7 +97,6 @@ const String template = r'''<?xml version="1.0" encoding="UTF-8"?>
                   <a:ln w="50800">
                     <a:noFill/>
                   </a:ln>
->>>>>>> Stashed changes
                 </p:spPr>
             </p:pic>
             <!-- Photo 3 -->
@@ -145,18 +125,6 @@ const String template = r'''<?xml version="1.0" encoding="UTF-8"?>
                 </p:blipFill>
                 <p:spPr>
                   <a:xfrm>
-<<<<<<< Updated upstream
-                    <a:off x="16284702" y="679163"/>
-                    <a:ext cx="6567291" cy="11020500"/>
-                  </a:xfrm>
-                  <a:prstGeom prst="rect">
-                    <a:avLst/>
-                  </a:prstGeom>
-                </p:spPr>
-            </p:pic>
-<<<<<<< HEAD
-=======
-=======
                     <a:off x="16284702" y="2540000"/>
                     <a:ext cx="6200000" cy="9500000"/>
                   </a:xfrm>
@@ -168,20 +136,12 @@ const String template = r'''<?xml version="1.0" encoding="UTF-8"?>
                   </a:ln>
                 </p:spPr>
             </p:pic>
->>>>>>> Stashed changes
             <!-- Title on top layer -->
             {{#title}}
             <p:sp>
                 <p:nvSpPr>
                     <p:cNvPr id="{{new-id}}" name="SLIDE TITLE"/>
                     <p:cNvSpPr txBox="1"/>
-<<<<<<< Updated upstream
-                    <p:nvPr/>
-                </p:nvSpPr>
-                <p:spPr>
-                    <a:xfrm>
-                        <a:off x="1070250" y="1024200"/>
-=======
                     <p:nvPr>
                         <p:ph type="title"/>
                     </p:nvPr>
@@ -189,84 +149,22 @@ const String template = r'''<?xml version="1.0" encoding="UTF-8"?>
                 <p:spPr>
                     <a:xfrm>
                         <a:off x="3042330" y="490500"/>
->>>>>>> Stashed changes
-                        <a:ext cx="10800000" cy="400000"/>
+                        <a:ext cx="10800000" cy="1270000"/>
                     </a:xfrm>
                     <a:prstGeom prst="rect">
                         <a:avLst/>
                     </a:prstGeom>
                 </p:spPr>
                 <p:txBody>
-<<<<<<< Updated upstream
                     <a:bodyPr anchor="t"/>
                     <a:lstStyle>
-                        <a:lvl1pPr algn="l">
-                            <a:defRPr sz="8400"/>
-                        </a:lvl1pPr>
-=======
-                    <a:bodyPr anchor="ctr"/>
-                    <a:lstStyle>
                         <a:lvl1pPr algn="ctr"/>
->>>>>>> Stashed changes
                     </a:lstStyle>
                     {{>text-value}}
                 </p:txBody>
             </p:sp>
             {{/title}}
 
-<<<<<<< Updated upstream
-            <!-- Description body text -->
-            {{#description}}
-            <p:sp>
-                <p:nvSpPr>
-                    <p:cNvPr id="{{new-id}}" name="Description"/>
-                    <p:cNvSpPr txBox="1">
-                        <a:spLocks noGrp="1"/>
-                    </p:cNvSpPr>
-                    <p:nvPr>
-                        <p:ph type="body" idx="1"/>
-                    </p:nvPr>
-                </p:nvSpPr>
-                <p:spPr>
-                    <a:prstGeom prst="rect">
-                        <a:avLst/>
-                    </a:prstGeom>
-                </p:spPr>
-                <p:txBody>
-                    <a:bodyPr/>
-                    <a:lstStyle/>
-                    {{>text-value}}
-                </p:txBody>
-            </p:sp>
-            {{/description}}
-
-            <!-- Description body text -->
-            {{#description}}
-            <p:sp>
-                <p:nvSpPr>
-                    <p:cNvPr id="{{new-id}}" name="Description"/>
-                    <p:cNvSpPr txBox="1">
-                        <a:spLocks noGrp="1"/>
-                    </p:cNvSpPr>
-                    <p:nvPr>
-                        <p:ph type="body" idx="1"/>
-                    </p:nvPr>
-                </p:nvSpPr>
-                <p:spPr>
-                    <a:prstGeom prst="rect">
-                        <a:avLst/>
-                    </a:prstGeom>
-                </p:spPr>
-                <p:txBody>
-                    <a:bodyPr/>
-                    <a:lstStyle/>
-                    {{>text-value}}
-                </p:txBody>
-            </p:sp>
-            {{/description}}
-
-=======
->>>>>>> Stashed changes
             <!-- Page number as image next to the logo (top-right) -->
             {{#imageId4}}
             <p:pic>
@@ -303,11 +201,7 @@ const String template = r'''<?xml version="1.0" encoding="UTF-8"?>
                 <p:nvPicPr>
                     <p:cNvPr id="{{new-id}}" name="Logo" descr="Company Logo"/>
                     <p:cNvPicPr>
-<<<<<<< Updated upstream
-                        <a:picLocks noGrp="1" noChangeAspect="1"/>
-=======
                         <a:picLocks noGrp="1"/>
->>>>>>> Stashed changes
                     </p:cNvPicPr>
                     <p:nvPr/>
                 </p:nvPicPr>
@@ -331,20 +225,11 @@ const String template = r'''<?xml version="1.0" encoding="UTF-8"?>
             </p:pic>
             {{/imageId5}}
 
-<<<<<<< Updated upstream
->>>>>>> b174b2bc5d3c8c08ac0da99d526146eded274d6d
-=======
->>>>>>> Stashed changes
             {{>speaker-notes}}
         </p:spTree>
     </p:cSld>
     <p:clrMapOvr>
         <a:masterClrMapping/>
     </p:clrMapOvr>
-<<<<<<< HEAD
     <p:transition xmlns:p14="http://schemas.microsoft.com/office/powerpoint/2010/main" spd="med" advClick="1"/>
-</p:sld>''';''';
-=======
-    <p:transition spd="med"/>
 </p:sld>''';
->>>>>>> b174b2bc5d3c8c08ac0da99d526146eded274d6d
