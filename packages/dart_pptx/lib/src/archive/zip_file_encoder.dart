d94db5259d72495f993d8fa46210e07f231b5d5a
fajusto <fajusto@icloud.com>
2026-05-25 16:06:19 -0300

    fix(dart_pptx): omit directory entries from pptx zip

--- packages/dart_pptx/lib/src/archive/zip_file_encoder.dart
+++ packages/dart_pptx/lib/src/archive/zip_file_encoder.dart
@@ -29,8 +29,12 @@ class ZipFileEncoder {
     final zipPath = filename ?? '$dirPath.zip';
     level ??= GZIP;
     create(zipPath, level: level, modified: modified);
-    addDirectory(dir,
-        includeDirName: false, level: level, followLinks: followLinks);
+    addDirectory(
+      dir,
+      includeDirName: false,
+      level: level,
+      followLinks: followLinks,
+    );
     close();
   }
 
@@ -54,33 +58,28 @@ class ZipFileEncoder {
     final files = dir.listSync(recursive: true, followLinks: followLinks);
     var futures = <Future<void>>[];
     for (var file in files) {
-      if (file is Directory) {
-        var filename = path.relative(file.path, from: dir.path);
-        filename = includeDirName ? ('$dirName/$filename') : filename;
-        final af = ArchiveFile('$filename/', 0, null);
-        af.mode = file.statSync().mode;
-        af.lastModTime =
-            file.statSync().modified.millisecondsSinceEpoch ~/ 1000;
-        af.isFile = false;
-        _encoder.addFile(af);
-      } else if (file is File) {
+      if (file is File) {
         final dirName = path.basename(dir.path);
         final relPath = path.relative(file.path, from: dir.path);
-        futures.add(addFile(
-            file, includeDirName ? ('$dirName/$relPath') : relPath, level));
+        futures.add(
+          addFile(
+            file,
+            includeDirName ? ('$dirName/$relPath') : relPath,
+            level,
+          ),
+        );
       }
     }
     await Future.wait(futures);
   }
 
-  Future<void> addFile(
-    File file, [
-    String? filename,
-    int? level = GZIP,
-  ]) async {
+  Future<void> addFile(File file, [String? filename, int? level = GZIP]) async {
     var fileStream = InputFileStream(file.path);
     var archiveFile = ArchiveFile.stream(
-        filename ?? path.basename(file.path), file.lengthSync(), fileStream);
+      filename ?? path.basename(file.path),
+      file.lengthSync(),
+      fileStream,
+    );
 
     if (level == STORE) {
       archiveFile.compress = false;
